088de7574429a023fa10782f802526fafefd4cf9
David <wilson.davidwilson@gmail.com>
2023-10-21 13:40:04 +0100

    update to latest version

--- src/test/java/serenityswag/authentication/LoginActions.java
+++ src/test/java/serenityswag/authentication/LoginActions.java
@@ -1,11 +1,11 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.steps.UIInteractionSteps;
-import net.thucydides.core.annotations.Step;
 
 public class LoginActions extends UIInteractionSteps {
     @Step("Log in as {0}")
-    public void as(User user) {
+    public void asUser(User user) {
         openUrl("https://www.saucedemo.com/");
 
         // Login as a standard user
--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,9 +1,9 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -26,7 +26,7 @@ public class WhenLoggingOn {
     @Test
     public void usersCanLogOnViaTheHomePage() {
 
-        login.as(STANDARD_USER);
+        login.asUser(STANDARD_USER);
 
         // Should see product catalog
         Serenity.reportThat("The inventory page should be displayed with the correct title",
